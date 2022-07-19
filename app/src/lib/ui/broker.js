8b160547a9adfe04a25dee45c4d8b56a21316ef9
indexpro24@gmail.com <indexpro24@gmail.com>
2022-07-19 21:50:23 +0300

    git init

--- app/src/lib/ui/broker.js
+++ app/src/lib/ui/broker.js
@@ -1,12 +1,12 @@
-export { default as Mobile_Menu_v1 } from './mobile_menu/section_01/index.svelte';
+export { default as Mobile_Menu_v1 } from './mobile_menu/01/index.svelte';
 
-export { default as Header_v1 } from './header/section_01/index.svelte';
-export { default as Header_v2 } from './header/section_02/index.svelte';
+export { default as Header_v1 } from './header/01/index.svelte';
+export { default as Header_v2 } from './header/02/index.svelte';
 
-export { default as Footer_v1 } from './footer/section_01/index.svelte';
+export { default as Footer_v1 } from './footer/01/index.svelte';
 
-export { default as Banner_v1 } from './banners/section_01/index.svelte';
+export { default as Banner_v1 } from './banners/01/index.svelte';
 
-export { default as Marketing_v1 } from './marketing/section_01/index.svelte';
+export { default as Marketing_v1 } from './marketing/01/index.svelte';
 
 
