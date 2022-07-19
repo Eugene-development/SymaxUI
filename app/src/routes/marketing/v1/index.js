a896ce57976ba8b4cc2053a396a04cc53e70ced3
indexpro24@gmail.com <indexpro24@gmail.com>
2022-07-19 21:24:21 +0300

    git init

---
+++ app/src/routes/marketing/v1/index.js
@@ -0,0 +1,54 @@
+const img_1 = 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV1/kitchen.jpeg';
+const alt_1 = ''
+const img_2 = 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV1/shkaf.jpeg';
+const alt_2 = '';
+const title = 'Lorem ipsum dolor sit amet';
+const description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, provident saepe. Alias error inventore magni optio quaerat quasi repellendus temporibus!';
+const benefits_1 = [
+    {
+        value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, provident saepe. Alias error inventore magni optio quaerat quasi repellendus temporibus!',
+    },
+    {
+        value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, provident saepe. Alias error inventore magni optio quaerat quasi repellendus temporibus!',
+    },
+    {
+        value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, provident saepe. Alias error inventore magni optio quaerat quasi repellendus temporibus!',
+    },
+    {
+        value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, provident saepe. Alias error inventore magni optio quaerat quasi repellendus temporibus!',
+    },
+];
+
+const benefits_2 = [
+    {
+        value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, provident saepe. Alias error inventore magni optio quaerat quasi repellendus temporibus!',
+    },
+    {
+        value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, provident saepe. Alias error inventore magni optio quaerat quasi repellendus temporibus!',
+    },
+    {
+        value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, provident saepe. Alias error inventore magni optio quaerat quasi repellendus temporibus!',
+    },
+    {
+        value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, provident saepe. Alias error inventore magni optio quaerat quasi repellendus temporibus!',
+    },
+];
+
+const contentMarketing_v1 = {
+    img_1,
+    alt_1,
+    img_2,
+    alt_2,
+    title,
+    description,
+    benefits_1,
+    benefits_2
+};
+
+export const get = () => {
+    return {
+        body: {
+            contentMarketing_v1
+        }
+    };
+};
---
+++ app/src/routes/marketing/v1/index.svelte
@@ -0,0 +1,6 @@
+<script>
+    import {Marketing_v1} from "../../../lib/ui/broker.js";
+    export let contentMarketing_v1
+</script>
+
+<Marketing_v1 {...contentMarketing_v1}/>
