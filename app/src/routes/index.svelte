92570846414e2388f076ed084b41c4393c8f7e43
indexpro24@gmail.com <indexpro24@gmail.com>
2022-07-29 19:58:55 +0300

    git init

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,4 +1,34 @@
 <script>
 </script>
 
-<p>Body</p>
+
+
+<a href="/article/v1">article/v1</a>
+<br>
+
+<a href="/banner/v1">banner/v1</a>
+<br>
+
+<a href="/blog/v1">blog/v1</a>
+<br>
+
+<a href="/contact/v1">contact/v1</a>
+<br>
+
+<a href="/footer/v1">footer/v1</a>
+<br>
+
+<a href="/header/v1">header/v1</a>
+<br>
+<a href="/header/v2">header/v2</a>
+<br>
+
+<a href="/main/v1">main/v1</a>
+<br>
+
+<a href="/marketing/v1">marketing/v1</a>
+<br>
+<a href="/marketing/v2">marketing/v2</a>
+<br>
+
+
