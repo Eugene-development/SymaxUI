28c6868b3ac162a692efe1d2ff3ee8e910e635b5
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-18 20:58:44 +0300

    git init

--- app/src/lib/ui/header/03/index.svelte
+++ app/src/lib/ui/header/03/index.svelte
@@ -7,6 +7,9 @@
 	export let pathImg;
 	export let title;
 	export let description;
+	export let parentPage;
+	export let parentPageLink;
+	export let currentPage;
 </script>
 
 <div class="relative">
@@ -17,11 +20,10 @@
 	<div class="relative max-w-7xl mx-auto py-24 px-4 sm:py-24 sm:px-6 lg:px-8">
 
 
-
 			<ol class="max-w-screen-xl w-full mx-auto pb-16 flex space-x-4">
 				<li class="flex">
 					<div class="flex items-center">
-						<a href="#" class="text-gray-100 hover:text-gray-200">
+						<a href="/" class="text-gray-100 hover:text-gray-200">
 							<!-- Heroicon name: solid/home -->
 							<svg class="flex-shrink-0 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 								<path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
@@ -31,21 +33,23 @@
 					</div>
 				</li>
 
-				<li class="flex">
-					<div class="flex items-center">
-						<svg class="flex-shrink-0 w-6 h-full text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-							<path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
-						</svg>
-						<a href="#" class="ml-4 text-sm font-medium text-gray-100 hover:text-gray-200">Projects</a>
-					</div>
-				</li>
+				{#if (parentPage)}
+					<li class="flex">
+						<div class="flex items-center">
+							<svg class="flex-shrink-0 w-6 h-full text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+								<path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+							</svg>
+							<a href="{parentPageLink}" class="ml-4 text-sm font-medium text-gray-100 hover:text-gray-200">{parentPage}</a>
+						</div>
+					</li>
+				{/if}
 
 				<li class="flex">
 					<div class="flex items-center">
 						<svg class="flex-shrink-0 w-6 h-full text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
 							<path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
 						</svg>
-						<a href="#" class="ml-4 text-sm font-medium text-gray-100 hover:text-gray-200" aria-current="page">Project Nero</a>
+						<span class="ml-4 text-sm font-medium text-gray-100 hover:text-gray-200" aria-current="page">{currentPage}</span>
 					</div>
 				</li>
 			</ol>
--- app/src/routes/header/v3/index.js
+++ app/src/routes/header/v3/index.js
@@ -1,9 +1,15 @@
 const title = 'Шкафы в Москве и области';
 const description = 'Шкафы в Москве под ваши размеры. Дизайн проект в подарок.';
+const parentPage = ''
+const parentPageLink = ''
+const currentPage = 'Текущая'
 const pathImg =
 	'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/1.jpg';
 
 const content_Header_v3 = {
+	parentPage,
+	parentPageLink,
+	currentPage,
 	title,
 	description,
 	pathImg
