ed8775b648d0f272803cf2495aa27e68415e9bc3
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-29 18:29:38 +0300

    git init

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,26 +1,60 @@
 <script>
-    import { request, gql } from 'graphql-request'
+//     import { request, gql } from 'graphql-request'
+//
+//     const query = gql`
+//       query ProductsWhere {
+//       product_where(key: 1234) {
+//         id
+//         key
+//         text: value
+//         property {
+//           value
+//         }
+//         parentable {
+//           ... on Category {
+//             value
+//           }
+//
+//         }
+//       }
+//     }
+// `
+//
+//     request('http://127.0.0.1:8001/graphql', query).then((data) => console.log(data))
 
-    const query = gql`
-  query ProductsWhere {
-  product_where(key: 1234) {
-    id
-    key
-    text: value
-    property {
-      value
-    }
-    parentable {
-      ... on Category {
-        value
-      }
 
-    }
-  }
-}
-`
+    fetch('http://127.0.0.1:8001/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: `
+                query ProductsWhere {
+                  product_where(key: 1234) {
+                    id
+                    key
+                    text: value
+                    property {
+                      value
+                    }
+                    parentable {
+                      ... on Category {
+                        value
+                      }
+                }
+              }
+            }
+      `,
+            variables: {},
+        }),
+    })
+        .then((res) => res.json())
+        .then((result) => console.log(result));
 
-    request('http://127.0.0.1:8001/graphql', query).then((data) => console.log(data))
+
+
+
 </script>
 <a href="/article/v1">article/v1</a>
 <br />
